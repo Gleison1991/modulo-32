285653df547a32880d1fa10d8c66295f686d9c78
gleison <gleisondeivison3@gmail.com>
2023-08-31 18:24:01 -0300

    Criando os primeiros components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import ListaDeContatos from './containers/ListaDeContatos'
+import EstiloGlobal, { Container } from './styles'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Olá</h1>
-    </div>
+    <>
+      <EstiloGlobal />
+      <Container>
+        <ListaDeContatos />
+      </Container>
+    </>
   )
 }
 
